81b39428c15ae61ee7d52f7a3b57c2925010d420
Parfa Longs <longsparfa@gmail.com>
2024-07-01 14:40:14 +0100

    update touch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Barry from "./otters/otter1.jpg";
+import Domnan from "./otters/otter2.jpg";
+import Seljul from "./otters/otter3.jpg";
+import Domfa from "./otters/otter4.jpg";
+import Julfa from "./otters/otter5.jpg";
+import Header from "./components/Header";
+import PostList from "./components/PostList";
+import SelectedItem from "./components/SelectedItem";
+import { useState } from "react";
 
 function App() {
+  const [selectedPost, setSelectedPost] = useState({
+    id: 1,
+    src: Barry,
+    title: "Barry",
+  });
+
+  function handleClick(otter) {
+    setSelectedPost(otter);
+  }
+  const ottersArray = [
+    { id: 1, src: Barry, title: "Barry" },
+    { id: 2, src: Domfa, title: "Domfa" },
+    { id: 3, src: Domnan, title: "Domnan" },
+    { id: 4, src: Seljul, title: "Seljul" },
+    { id: 5, src: Julfa, title: "Julfa" },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="app-content">
+        <ul className="post-list">
+          {ottersArray.map((otter) => {
+            return (
+              <PostList
+                key={otter.id}
+                id={otter.id}
+                src={otter.src}
+                title={otter.title}
+                onSelect={handleClick}
+              />
+            );
+          })}
+        </ul>
+        <SelectedItem src={selectedPost?.src} title={selectedPost?.title} />
+      </div>
     </div>
   );
 }
